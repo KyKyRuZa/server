66ae682ba467d5ea8167675b07980dd9651b89e2
KyKyRuZa <ituhvatullin8@gmail.com>
2025-03-22 14:59:00 +0300

    fw

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -8,7 +8,7 @@ const crypto = require('crypto');
 
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.delron.ru',
+    host: 'smtp.mail.ru',
     port: 465,
     secure: true,
     auth: {
